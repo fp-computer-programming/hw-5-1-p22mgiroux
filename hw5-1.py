03ffb830eeade613bf19a7385bc38cc6ab994766
p22mgiroux <p22mgiroux@fairfieldprep.org>
2021-10-22 09:59:30 -0400

    make progress on hw5-1

---
+++ hw5-1.py
@@ -0,0 +1,33 @@
+# Author: MOG 10/22/21
+import random
+random.seed(16)
+
+# Question 1
+print(random.randint(31, 49))
+
+# Question 2
+print(random.randrange(4, 9, 2))
+
+# Question 3
+print(random.randrange(21, 30, 2))
+
+# Question 4
+print(random.randint(1, 8))
+
+# Question 5
+print(random.randint(1, 20))
+
+# Question 6
+print(random.choice(['cat', 'dog', 'sheep', 'cow', 'chicken', 'pig']))
+
+# Question 7
+print(random.choices([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], k = 4))
+
+# Question 8
+print(random.shuffle([1, 2, 3, 4, 5]))
+
+# Question 9
+print()
+
+# Question 10
+print()
